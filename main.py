605d392b213ffa48573d178bb9e088d225bf165a
lladinog <159490210+lladinog@users.noreply.github.com>
2024-09-06 00:39:17 -0500

    up

--- main.py
+++ main.py
@@ -29,30 +29,37 @@ def resultado():
 root = Tk()
 root.title("Calculadora POO")
 root.resizable(0, 0)
-root.geometry("500x300")
+root.geometry("350x300")  # Ajusta el tamaño de la ventana
 
 # Configuración pantalla de salida 
-pantalla = Entry(root, width=40, bg="black", fg="white", borderwidth=0, font=("arial", 18, "bold"))
-pantalla.grid(row=0, column=0, columnspan=4, padx=1, pady=1)
+pantalla = Entry(root, width=33, bg="black", fg="white", borderwidth=0, font=("arial", 18, "bold"))
+pantalla.grid(row=0, column=0, columnspan=4, padx=1, pady=1, sticky="nsew")
 
 # Configuración botones
-Button(root, text="1", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(1)).grid(row=1, column=0, padx=1, pady=1)
-Button(root, text="2", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(2)).grid(row=1, column=1, padx=1, pady=1)
-Button(root, text="3", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(3)).grid(row=1, column=2, padx=1, pady=1)
-Button(root, text="+", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: operar("+")).grid(row=1, column=3, padx=1, pady=1)
+Button(root, text="1", bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(1)).grid(row=1, column=0, sticky="nsew",padx=1,pady=1)
+Button(root, text="2", bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(2)).grid(row=1, column=1, sticky="nsew",padx=1,pady=1)
+Button(root, text="3", bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(3)).grid(row=1, column=2, sticky="nsew",padx=1,pady=1)
+Button(root, text="+", bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: operar("+")).grid(row=1, column=3, sticky="nsew",padx=1,pady=1)
 
-Button(root, text="4", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(4)).grid(row=2, column=0, padx=1, pady=1)
-Button(root, text="5", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(5)).grid(row=2, column=1, padx=1, pady=1)
-Button(root, text="6", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(6)).grid(row=2, column=2, padx=1, pady=1)
-Button(root, text="-", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: operar("-")).grid(row=2, column=3, padx=1, pady=1)
+Button(root, text="4", bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(4)).grid(row=2, column=0, sticky="nsew",padx=1,pady=1)
+Button(root, text="5", bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(5)).grid(row=2, column=1, sticky="nsew",padx=1,pady=1)
+Button(root, text="6", bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(6)).grid(row=2, column=2, sticky="nsew",padx=1,pady=1)
+Button(root, text="-", bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: operar("-")).grid(row=2, column=3, sticky="nsew",padx=1,pady=1)
 
-Button(root, text="7", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(7)).grid(row=3, column=0, padx=1, pady=1)
-Button(root, text="8", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(8)).grid(row=3, column=1, padx=1, pady=1)
-Button(root, text="9", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(9)).grid(row=3, column=2, padx=1, pady=1)
-Button(root, text="*", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: operar("*")).grid(row=3, column=3, padx=1, pady=1)
+Button(root, text="7", bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(7)).grid(row=3, column=0, sticky="nsew",padx=1,pady=1)
+Button(root, text="8", bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(8)).grid(row=3, column=1, sticky="nsew",padx=1,pady=1)
+Button(root, text="9", bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: agregar_valor(9)).grid(row=3, column=2, sticky="nsew",padx=1,pady=1)
+Button(root, text="*", bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: operar("*")).grid(row=3, column=3, sticky="nsew",padx=1,pady=1)
 
-Button(root, text="=", width=20, height=3, bg="red", fg="white", borderwidth=0, cursor="hand2", command=resultado).grid(row=4, column=0, columnspan=2, padx=1, pady=1)
-Button(root, text=".", width=9, height=3, bg="spring green", fg="black", cursor="hand2", borderwidth=0, command=lambda: agregar_valor(".")).grid(row=4, column=2, padx=1, pady=1)
-Button(root, text="/", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: operar("/")).grid(row=4, column=3, padx=1, pady=1)
+Button(root, text="=", bg="red", fg="white", borderwidth=0, cursor="hand2", command=resultado).grid(row=4, column=0, columnspan=2, sticky="nsew",padx=1,pady=1)
+Button(root, text=".", bg="spring green", fg="black", cursor="hand2", borderwidth=0, command=lambda: agregar_valor(".")).grid(row=4, column=2, sticky="nsew",padx=1,pady=1)
+Button(root, text="/", bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: operar("/")).grid(row=4, column=3, sticky="nsew",padx=1,pady=1)
+
+# Configuración para expandir columnas y filas uniformemente
+for i in range(4):
+    root.grid_columnconfigure(i, weight=1)
+for i in range(5):
+    root.grid_rowconfigure(i, weight=1)
+
 
 root.mainloop()
